64c66b6a819e5242975adec5a6bc34bd614422e6
kokusz00 <dnrradar@gmail.com>
2025-06-13 15:09:30 +0200

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,1 +1,18 @@
+function getCallouts() {
+  return JSON.parse(localStorage.getItem('callouts') || '[]');
+}
+
+function addCallout(name, location) {
+  const callouts = getCallouts();
+  callouts.push({ name, location });
+  localStorage.setItem('callouts', JSON.stringify(callouts));
+  localStorage.setItem('calloutUpdated', Date.now()); // signal update
+}
+
+function removeCallout(index) {
+  const callouts = getCallouts();
+  callouts.splice(index, 1);
+  localStorage.setItem('callouts', JSON.stringify(callouts));
+  localStorage.setItem('calloutUpdated', Date.now()); // signal update
+}
 
